a519b96668dc00669b07290e414cf2596839b335
Paul Kim <paulkim0516@gmail.com>
2023-05-05 04:46:57 +0900

    Switch research completing logic
    Switch to a method that shows researched item correctly but takes time
    Add code to prevent button functions before ongoing research process is finished

--- index.ts
+++ index.ts
@@ -11,6 +11,12 @@ import
 from "openrct2-flexui";
 
 const getMaxCount = (maxCount: number) => { return Math.min(maxCount, park.research.uninventedItems.length); }
+
+// flag indicating availability for new action
+let isStandby = true;
+// handle of interval function executed on standby
+let standbyHandle: number;
+
 function shuffle<T>(array: T[]): T[] {
     let currentIndex = array.length,  randomIndex;
 
@@ -30,27 +36,64 @@ function shuffle<T>(array: T[]): T[] {
 };
 
 const pickResearch = () => {
-	let inventedItems: ResearchItem; 
-	let newUninventedItems: ResearchItem[] = [];
-	
 	if(park.cash < (price * 10)) {
 		ui.showError("자금 부족","뽑기를 수행할 돈이 부족합니다.");
+	} else if (park.research.uninventedItems.length == 0) {
+		ui.showError("연구 불가", "더 이상 연구를 할 수 없습니다.");
 	} else {
 		park.cash -= (price * 10);
 
 		if (Math.random() < (prob / 100)) {
-			park.research.uninventedItems = shuffle(park.research.uninventedItems);
-			inventedItems = park.research.uninventedItems[0];
-			newUninventedItems = park.research.uninventedItems.slice(1);
-			park.research.inventedItems = [...park.research.inventedItems, inventedItems];
-			park.research.uninventedItems = newUninventedItems;
-
-			if(inventedItems.type == "ride") park.postMessage({type:"research", text:"{GREEN}새로운 탈것을 뽑았습니다!"});
-			else park.postMessage({type:"research", text:"{GREEN}새로운 풍경을 뽑았습니다!"});
+			// if on initial research, finish initial research and move on to completing research
+			// this step is necessary since OpenRCT2 sets park.research.expectedItem only after initial research
+			if (park.research.stage === "initial_research") {
+				initialResearch();
+			} else {
+				completeResearch();
+			}
 		} else {
 			park.postMessage({type:"blank", text:"{RED}가챠 실패로 아무것도 얻지 못했습니다..."});
 		}
 	}
+}
+
+const onStandby = () => {
+	park.research.funding = 0; // ResearchFundingLevel.None
+	park.research.progress = 0;
+}
+
+// set progress to maximum to minimize research time 
+const initialResearch = () => {
+	isStandby = false;
+	// clear interval to prevent configuration collision
+	context.clearInterval(standbyHandle);
+
+	park.research.funding = 3; // ResearchFundingLevel.Maximum
+	park.research.progress = 65535;
+	let handle = context.setInterval(() => {
+		// probe to see if research moved to next stage
+		// if on next stage, execute function to finish research and remove probe
+		if (park.research.stage === "designing") {
+			completeResearch();
+			context.clearInterval(handle);
+		}
+	}, 500); // 20 tick
+}
+
+// skip designing stage and set state to be right before finishing research
+const completeResearch = () => {
+	isStandby = false;
+	park.research.funding = 3; // ResearchFundingLevel.Maximum
+	park.research.stage = "completing_design";
+	park.research.progress = 65535;
+	park.research.uninventedItems = shuffle(park.research.uninventedItems);
+	let handle = context.setInterval(() => {
+		if (!park.research.expectedItem) {
+			isStandby = true;
+			standbyHandle = context.setInterval(onStandby, 1000);
+			context.clearInterval(handle);
+		}
+	}, 500); // 20 tick
 }
 
 const gachaWindow = window({
@@ -68,19 +111,38 @@ const gachaWindow = window({
 				button({
 					text: "1뽑",
 					height: "28px",
-					onClick: () => {pickResearch()}
+					// allow picking only when standby
+					onClick: () => {
+						isStandby ? pickResearch() : ui.showError("연구 불가", "뽑기가 진행중 입니다.");
+					}
 				}),
 				button({
 					text: `${getMaxCount(10)}뽑`,
 					height: "28px",
+					// allow picking only when standby
 					onClick: () => {
-						const maxCount = getMaxCount(10);
-						if(park.cash < (maxCount * (price * 10))) {
-							ui.showError("자금 부족","뽑기를 수행할 돈이 부족합니다.");
-						}
-
-						for (let i = 0; i < maxCount; i++) {
-							pickResearch();
+						if (isStandby) {
+							const maxCount = getMaxCount(10);
+							if(park.cash < (maxCount * (price * 10))) {
+								ui.showError("자금 부족","뽑기를 수행할 돈이 부족합니다.");
+							} else if (park.research.uninventedItems.length == 0) {
+								ui.showError("연구 불가", "더 이상 연구를 할 수 없습니다.");
+							} else {
+								let i = 0;
+								let handle = context.setInterval(() => {
+									if (isStandby) {
+										pickResearch();
+										i++;
+									} else {
+										// attempt to complete unsuccessful research
+										completeResearch();
+									}
+									
+									if (i >= maxCount) context.clearInterval(handle);
+								}, 1600);
+							}
+						} else {
+							ui.showError("연구 불가", "뽑기가 진행중 입니다.");
 						}
 					}
 				})
@@ -89,11 +151,10 @@ const gachaWindow = window({
 	]
 });
 
-
 registerPlugin({
 	name: "오픈롤코 가챠",
 	version: "0.1.0",
-	authors: ["Lastorder-DC"],
+	authors: ["Lastorder-DC", "paulkim0516"],
 	type: "remote",
 	licence: "MIT",
 	targetApiVersion: 70,
@@ -101,7 +162,7 @@ registerPlugin({
 	{
 		// 로컬 플레이시만 동작시킨다
 		if(network.mode == "none") {
-			context.setInterval(() => {park.research.funding = 0;}, 1000)
+			standbyHandle = context.setInterval(onStandby, 1000);
 			ui.registerMenuItem("연구 가챠", () => gachaWindow.open());
 		}
 	}
